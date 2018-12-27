e429f066fb4fb6fafb212bad2a347b18fd3f9c5c
Asher Dawes <asher.dawes@gmail.com>
2018-12-26 20:19:23 -0800

    adding header to define __m128i

--- nheqminer/crypto/verus_hash.cpp
+++ nheqminer/crypto/verus_hash.cpp
@@ -11,7 +11,7 @@ bit output.
 #include <string.h>
 #include "crypto/common.h"
 #include "crypto/verus_hash.h"
-
+#include <x86intrin.h>
 void (*CVerusHash::haraka512Function)(unsigned char *out, const unsigned char *in);
 
 void CVerusHash::Hash(void *result, const void *data, size_t _len)
